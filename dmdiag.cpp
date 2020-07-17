ec77f8ec521d5766c885c5848bfd414ce22f6443
William Wallace <me@wiox.me>
2020-07-17 23:15:31 +0100

    script poc

--- dmdiag.cpp
+++ dmdiag.cpp
@@ -1,6 +1,7 @@
 #include <Windows.h>
 #include "dm/internal_types.h"
 #include "dm/State.h"
+#include <iostream>
 
 #include "squirrel.h"
 #include "sqstdaux.h"
@@ -8,6 +9,7 @@
 #include "sqstdmath.h"
 #include "sqstdstring.h"
 #include "sqstdsystem.h"
+#include <string>
 
 static const char dump_path[] = "C:\\Users\\wwall\\Downloads\\Dump\\core.80099";
 
@@ -37,6 +39,53 @@ int compile_file(HSQUIRRELVM v,const char *filename)
 		return 1;
 	}
 	return 0;
+}
+
+static void PushValue(HSQUIRRELVM v, dm::Value val)
+{
+	switch (val.type)
+	{
+	case dm::DataType::NUMBER:
+		sq_pushfloat(v, val._number);
+		break;
+	case dm::DataType::STRING:
+		sq_pushstring(v, val._string.string(), strlen(val._string.string()));
+		break;
+	case dm::DataType::MOB:
+		sq_pushroottable(v);
+		sq_pushstring(v, "Mob", 3);
+		sq_get(v, -2);
+		sq_pushroottable(v);
+		sq_pushinteger(v, val._data);
+		sq_call(v, 1, 0, SQFalse);
+		break;
+	default:
+		sq_pushroottable(v);
+		sq_pushstring(v, "UnknownType", 3);
+		sq_get(v, -2);
+		sq_pushroottable(v);
+		sq_pushinteger(v, (uint32_t) val.type);
+		sq_call(v, 2, 0, SQFalse);
+		break;
+	}
+}
+
+SQInteger GetField_Mob(HSQUIRRELVM v)
+{
+	
+	if (sq_gettop(v) != 3)
+		return 0;
+
+	SQInteger index;
+	const SQChar* key;
+	sq_getinteger(v, 2, &index);
+	sq_getstring(v, 3, &key);
+
+	dm::Ref<dm::Mob> mob{index};
+	dm::Ref<dm::String> str = *dm::current_state->GetStringRef(key);
+
+	PushValue(v, *dm::Mob::GetField(mob, str));
+	return 1;
 }
 
 int main(int argc, char** argv)
@@ -69,6 +118,11 @@ int main(int argc, char** argv)
 
 	sq_pushroottable(v);
 
+	sq_pushstring(v, "GetField_Mob", sizeof("GetField_Mob") - 1);
+	sq_newclosure(v, GetField_Mob, 0);
+	sq_newslot(v, -3, false);
+
+	/*
 	sq_pushstring(v, "UnknownType", sizeof("UnknownType") - 1);
 	sq_newclass(v, SQFalse);
 	sq_newslot(v, -3, SQFalse);
@@ -76,9 +130,32 @@ int main(int argc, char** argv)
 	sq_pushstring(v, "Mob", 3);
 	sq_newclass(v, SQFalse);
 	sq_newslot(v, -3, SQFalse);
+	*/
+
 	
+
 	if(SQ_FAILED(sqstd_dofile(v, _SC("E:\\dmdiag\\test.nut"), SQFalse, SQTrue))) {
 		printf("Error: failed to run test.nut\n");
+	}
+
+	while (true)
+	{
+		SQInteger oldtop=sq_gettop(v);
+
+		std::string str;
+		std::getline(std::cin, str);
+
+		
+		if(SQ_FAILED(sq_compilebuffer(v, str.c_str(), str.size(), "input", SQFalse)))
+		{
+			std::cout << "failed";
+			continue;
+		}
+
+		sq_pushroottable(v);
+		sq_call(v, 1, 0, SQTrue);
+		std::cout << '\n';
+		sq_settop(v, oldtop);
 	}
 
 	sq_pop(v, 1);
--- test.nut
+++ test.nut
@@ -1,1 +1,34 @@
-print("HELLO!")
+class UnknownType
+{
+	constructor(_type)
+	{
+		type = _type
+	}
+
+	function _tostring()
+	{
+		return "UnknownType(" + type + ")"
+	}
+
+	type = null
+}
+
+class Mob
+{
+	constructor(_index)
+	{
+		index = _index
+	}
+
+	function _get(key)
+	{
+		return ::GetField_Mob(index, key)
+	}
+
+	function _tostring()
+	{
+		return "Mob(" + index + ")"
+	}
+
+	index = null
+}
