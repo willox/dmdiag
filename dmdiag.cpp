fece2476d014b1c7a99693f13739dd6257ba2835
William Wallace <me@wiox.me>
2020-07-17 21:53:46 +0100

    squirrel wip

--- dmdiag.cpp
+++ dmdiag.cpp
@@ -2,7 +2,42 @@
 #include "dm/internal_types.h"
 #include "dm/State.h"
 
+#include "squirrel.h"
+#include "sqstdaux.h"
+#include "sqstdio.h"
+#include "sqstdmath.h"
+#include "sqstdstring.h"
+#include "sqstdsystem.h"
+
 static const char dump_path[] = "C:\\Users\\wwall\\Downloads\\Dump\\core.80099";
+
+void printfunc(HSQUIRRELVM v, const SQChar *s, ...) { 
+	va_list arglist; 
+	va_start(arglist, s); 
+	vprintf(s, arglist); 
+	va_end(arglist); 
+}
+
+SQInteger file_lexfeedASCII(SQUserPointer file)
+{
+	int ret;
+	char c;
+	if( ( ret=fread(&c,sizeof(c),1,(FILE *)file )>0) )
+		return c;
+	return 0;
+}
+
+int compile_file(HSQUIRRELVM v,const char *filename)
+{
+	FILE *f=fopen(filename,"rb");
+	if(f)
+	{
+		sq_compile(v,file_lexfeedASCII,f,filename,1);
+		fclose(f);
+		return 1;
+	}
+	return 0;
+}
 
 int main(int argc, char** argv)
 {
@@ -22,6 +57,32 @@ int main(int argc, char** argv)
 
 		continue;
 	}
+
+	HSQUIRRELVM v = sq_open(1024);
+	sqstd_seterrorhandlers(v);
+	sq_setprintfunc(v, printfunc, printfunc);
+	
+	sqstd_register_iolib(v);
+	sqstd_register_mathlib(v);
+	sqstd_register_stringlib(v);
+	sqstd_register_systemlib(v);
+
+	sq_pushroottable(v);
+
+	sq_pushstring(v, "UnknownType", sizeof("UnknownType") - 1);
+	sq_newclass(v, SQFalse);
+	sq_newslot(v, -3, SQFalse);
+
+	sq_pushstring(v, "Mob", 3);
+	sq_newclass(v, SQFalse);
+	sq_newslot(v, -3, SQFalse);
+	
+	if(SQ_FAILED(sqstd_dofile(v, _SC("E:\\dmdiag\\test.nut"), SQFalse, SQTrue))) {
+		printf("Error: failed to run test.nut\n");
+	}
+
+	sq_pop(v, 1);
+	sq_close(v);
 
 	return 0;
 }
